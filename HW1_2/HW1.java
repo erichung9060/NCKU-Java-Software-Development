bd8a3fac8bb61704375a298e0aa0e14a75de4d39
erichung1113 <erichung1113@gmail.com>
2024-03-13 09:51:21 +0800

    finished HW1 and HW2

--- HW1_2/HW1.java
+++ HW1_2/HW1.java
@@ -4,12 +4,13 @@ import java.util.Scanner;
 class HW1 {
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
-        
+
         BigDecimal num1 = scanner.nextBigDecimal();
         BigDecimal num2 = scanner.nextBigDecimal();
-        BigDecimal sum = num1.add(num2);
         
+        BigDecimal sum = num1.add(num2);
         System.out.println(sum);
+
         scanner.close();
     }
 }
